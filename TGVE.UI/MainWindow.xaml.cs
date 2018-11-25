d1031bd4fa4a3fdb7adf8075d0177f9059036fa8
Richard Sims <9633324@student.swin.edu.au>
2018-11-26 10:54:59 +1100

    Finished Challenge 3 Task 5.1

--- TGVE.UI/MainWindow.xaml.cs
+++ TGVE.UI/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TGVE.Api;
 using TGVE.WebApi.Models;
+using TMM.UI;
 
 namespace TGVE.UI
 {
@@ -103,6 +105,12 @@ namespace TGVE.UI
         private async Task EditClient()
         {
             Client client = (Client)DataView.SelectedItem;
+
+            if (!IsValidClient(client))
+            {
+                return;
+            }
+
             Client response = await clientsClient.PutAsync(client);
             MessageBox.Show($"Client {client.FirstName} {client.LastName} successfully changed", "Edit", MessageBoxButton.OK, MessageBoxImage.Information);
             return;
@@ -111,6 +119,12 @@ namespace TGVE.UI
         private async Task EditTour()
         {
             Tour tour = (Tour)DataView.SelectedItem;
+
+            if (!IsValidTour(tour))
+            {
+                return;
+            }
+
             Tour response = await toursClient.PutAsync(tour);
             MessageBox.Show($"Tour {tour.Name} at {tour.Location} successfully changed", "Edit", MessageBoxButton.OK, MessageBoxImage.Information);
             return;
@@ -119,6 +133,12 @@ namespace TGVE.UI
         private async Task AddClient()
         {
             Client client = (Client)DataView.SelectedItem;
+
+            if (!IsValidClient(client))
+            {
+                return;
+            }
+
             Client response = await clientsClient.PostAsync(client);
             List<Client> list = (List<Client>)DataView.ItemsSource;
             int lastAdded = list.FindIndex(s => s.Id == client.Id);
@@ -131,6 +151,12 @@ namespace TGVE.UI
         private async Task AddTour()
         {
             Tour tour = (Tour)DataView.SelectedItem;
+
+            if (!IsValidTour(tour))
+            {
+                return;
+            }
+
             Tour response = await toursClient.PostAsync(tour);
             List<Tour> list = (List<Tour>)DataView.ItemsSource;
             int lastAdded = list.FindIndex(s => s.Id == tour.Id);
@@ -158,6 +184,67 @@ namespace TGVE.UI
             list.Remove(tour);
             DataView.ItemsSource = list;
             MessageBox.Show($"Tour {tour.Name} at {tour.Location} successfully deleted", "Delete", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private bool IsValidClient(Client client)
+        {
+            try
+            {
+                if (DateTime.Now <= client.DateOfBirth)
+                {
+                    throw new ValidationFailureException($"{client.DateOfBirth} is not a valid date.");
+                }
+
+                Regex nameRegex = new Regex("^[a-zA-Z]*$");
+
+                if (!nameRegex.IsMatch(client.FirstName) || !nameRegex.IsMatch(client.LastName))
+                {
+                    throw new ValidationFailureException($"{client.FirstName} {client.LastName} is not a valid name.");
+                }
+
+                Regex phoneRegex = new Regex(@"\+\d* \d{3}\ *\d{3} \d{4}");
+
+                if (!phoneRegex.IsMatch(client.PhoneNumber))
+                {
+                    throw new ValidationFailureException($"{client.PhoneNumber} not valid. Phone number must be in +123 555 555 5555 format.");
+                }
+
+                MailAddress email = new MailAddress(client.Email);
+                return true;
+            }
+            catch (ValidationFailureException error)
+            {
+                MessageBox.Show($"Could not add Client\n\n{error.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return false;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show($"Could not add Client\n\n{error.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return false;
+            }
+        }
+
+        private bool IsValidTour(Tour tour)
+        {
+            try
+            {
+                if (DateTime.Now > tour.TourStartTime || DateTime.Now > tour.TourEndTime || (tour.TourStartTime >= tour.TourEndTime))
+                {
+                    throw new ValidationFailureException($"{tour.TourStartTime} or {tour.TourEndTime} is not a valid date.");
+                }
+
+                return true;
+            }
+            catch (ValidationFailureException error)
+            {
+                MessageBox.Show($"Could not add Tour\n\n{error.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return false;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show($"Could not add Tour\n\n{error.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return false;
+            }
         }
     }
 }
